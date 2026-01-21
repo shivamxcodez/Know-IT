8c1fee1ca945f5230801912f9bdb851052a755b4
shivamxcodez <shivamsharma7421@gmail.com>
2026-01-21 10:57:24 +0530

    setup database and tested workflow

--- server/requests.php
+++ server/requests.php
@@ -1,7 +1,27 @@
 <?php
 
+session_start();
+include("../common/db.php");
 if(isset($_POST['signup'])){
-    echo "User name is :".$_POST['username']."<br>";
+
+    $username=$_POST['username'];
+    $email=$_POST['email'];
+    $password=$_POST['password'];
+    $address=$_POST['address'];
+
+
+    $user=$conn->prepare("INSERT INTO `users`
+    (`id`,`username`,`email`,`password`,`address`)
+    VAlUES (NUll,'$username','$email','$password','$address')");
+
+    $result=$user->execute();
+    if($result){
+        echo "new user registered successfully";
+        $_SESSION["user"]=["username"=>$username, "email"=>$email];
+    }
+    else{
+        echo "error in registering new user";
+    }
 }
 
 
